6ef348433e9395f0cccca3c56e60b7ad9429b852
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-16 14:22:37 -0400

    fetch trending anime, undefined bug

--- src/components/containers/HomePageContainer.js
+++ src/components/containers/HomePageContainer.js
@@ -2,11 +2,33 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import HomePageView from "../views/HomePageView";
 import { fetchTrendingThunk } from "../../thunks";
+import PropTypes from "prop-types";
 
 class HomePageContainer extends Component {
+  componentDidMount() {
+    this.props.fetchTrending();
+  }
+
   render() {
-    return <HomePageView />;
+    return <HomePageView trendingAnime={this.props.trendingAnime} />;
   }
 }
 
-export default HomePageContainer;
+const mapState = (state) => {
+  return {
+    trendingAnime: state.trendingAnime,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    fetchTrending: () => dispatch(fetchTrendingThunk()),
+  };
+};
+
+HomePageContainer.propTypes = {
+  trendingAnime: PropTypes.array.isRequired,
+  fetchTrending: PropTypes.func.isRequired,
+};
+
+export default connect(mapState, mapDispatch)(HomePageContainer);
