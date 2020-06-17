b475da0bacabc952c38cc4e2b3d49ca91abf4976
Vivian Tom <viviantom@Vivians-Air.fios-router.home>
2020-06-16 21:39:24 -0400

    added axios call for animelist and updated reducer function

--- src/store/utilities/homepage.js
+++ src/store/utilities/homepage.js
@@ -2,12 +2,19 @@ import axios from "axios";
 
 //Action Types
 const FETCH_TRENDING = "FETCH_TRENDING";
-
+const FETCH_ANIME = "FETCH_ANIME";
 //ACTION CREATORS;
 const fetchTrending = (trendingAnime) => {
   return {
     type: FETCH_TRENDING,
     payload: trendingAnime,
+  };
+};
+
+const fetchAnime = (animeList) => {
+  return {
+    type: FETCH_ANIME,
+    payload: animeList,
   };
 };
 
@@ -26,11 +33,27 @@ export const fetchTrendingThunk = () => (dispatch) => {
     .catch((err) => console.log(err));
 };
 
+export const fetchAnimeListThunk = () => (dispatch) => {
+  return axios 
+  .get("https://kitsu.io/api/edge/anime/")
+  .then((res) => {
+    console.log("Anime thunk ", res.data);
+    return res.data;
+  })
+  .then((animeList) => {
+    return dispatch(fetchAnime(animeList));
+  })
+  .catch((err) => console.log(err));
+}
+
 //REDUCER
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_TRENDING:
       console.log("from reducer", action.payload);
+      return action.payload;
+    case FETCH_ANIME:
+      console.log("from reducer animeeee", action.payload);
       return action.payload;
     default:
       return state;
