84833ed7bf7040b33b9a8186634bc4e50f54e927
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-16 20:47:25 -0400

    added homepage carousel, installed bootstrap

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,11 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./HomePage.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import NavBarContainer from "../containers/NavBarContainer";
+import { Container } from "react-bootstrap";
 
 const HomePageView = (props) => {
   return (
     <div>
       {console.log("views ", typeof props.trendingAnime)}
-      <h1>{props.trendingAnime[0].attributes.slug}</h1>
+      <div>
+        <NavBarContainer />
+      </div>
+      <Container fluid>
+        <Carousel>
+          {props.trendingAnime.map((
+            anime,
+            index //add key //<a scr = "/:id"><img></a>
+          ) => (
+            <div>
+              <img
+                src={anime.attributes.coverImage.large}
+                alt={anime.attributes.titles.en_jp}
+              ></img>
+              <h5 className="legend">{anime.attributes.titles.en_jp}</h5>
+            </div>
+          ))}
+        </Carousel>
+      </Container>
     </div>
   );
 };
