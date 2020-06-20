b2233134f5c2dad81ae065391330ee2bd3b7514a
viviantom0506 <icemarina123@gmail.com>
2020-06-20 17:07:27 -0400

    added more styles

--- src/components/views/AnimeCategoriesPageView.js
+++ src/components/views/AnimeCategoriesPageView.js
@@ -7,22 +7,28 @@ const AnimeCategoriesPageView = props => {
 		console.log(props.anime);
 	}
 	return (
-		<div className="d-flex flex-wrap justify-content-around">
-			<h1></h1>
-			{props.anime.map(animeCategories => (
-				<div className="anime-cell">
-					<Link to={`/${animeCategories.id}`}>
-						{/* {console.log(animeCategories)} */}
-						<img
-							className="anime-image"
-							src={animeCategories.attributes.posterImage.small}
-							alt={animeCategories.attributes.slug}
-						/>
-					</Link>
-					<h4>{animeCategories.attributes.titles.en_jp}</h4>
-				</div>
-			))}
-		</div>
+		<>
+			<div className="d-flex flex-wrap justify-content-around">
+				<h1></h1>
+				{props.anime.map(animeCategories => (
+					<div className="anime-cell">
+						<Link to={`/${animeCategories.id}`}>
+							{/* {console.log(animeCategories)} */}
+							<img
+								className="anime-image"
+								src={animeCategories.attributes.posterImage.small}
+								alt={animeCategories.attributes.slug}
+							/>
+						</Link>
+						<div className="titles">
+							<h4 styles={{ fontSize: 5 }}>
+								{animeCategories.attributes.titles.en_jp}
+							</h4>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
 	);
 };
 
