8053d8e3f955c0b8ccece090670a6321a5346b71
viviantom0506 <icemarina123@gmail.com>
2020-06-21 18:06:34 -0400

    added missing s in http

--- src/store/utilities/user.js
+++ src/store/utilities/user.js
@@ -76,7 +76,7 @@ export const auth = (email, password, method) => async dispatch => {
 
 export const logout = () => async dispatch => {
 	try {
-		await axios.delete('http://zenime-server.herokuapp.com/auth/logout', {
+		await axios.delete('https://zenime-server.herokuapp.com/auth/logout', {
 			withCredentials: true,
 		});
 		dispatch(removeUser());
