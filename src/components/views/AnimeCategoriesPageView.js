4a1556ea1fce82e7c45dcb4616e5af8d2df44c71
viviantom0506 <icemarina123@gmail.com>
2020-06-21 19:31:14 -0400

    added title name turnery

--- src/components/views/AnimeCategoriesPageView.js
+++ src/components/views/AnimeCategoriesPageView.js
@@ -1,29 +1,33 @@
-import React from "react";
-import "./styling.css";
-import { Link } from "react-router-dom";
-import { Image } from "react-bootstrap";
+import React from 'react';
+import './styling.css';
+import { Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
 
-const AnimeCategoriesPageView = (props) => {
-  return (
-    <>
-      <div className="d-flex flex-wrap justify-content-around p-5 m-5">
-        {props.anime.map((anime) => (
-          <div className="anime-cells">
-            <Link to={`/${anime.id}`}>
-              <Image
-                className="anime-image cat-img"
-                src={anime.attributes.posterImage.medium}
-                alt={anime.attributes.slug}
-                rounded
-                fluid
-              />
-            </Link>
-            <p>{anime.attributes.titles.en_jp}</p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+const AnimeCategoriesPageView = props => {
+	return (
+		<>
+			<div className="d-flex flex-wrap justify-content-around p-5 m-5">
+				{props.anime.map(anime => (
+					<div className="anime-cells">
+						<Link to={`/${anime.id}`}>
+							<Image
+								className="anime-image cat-img"
+								src={anime.attributes.posterImage.medium}
+								alt={anime.attributes.slug}
+								rounded
+								fluid
+							/>
+						</Link>
+						<p>
+							{props.anime.attributes.title.en
+								? props.anime.attributes.title.en
+								: anime.attributes.titles.jp}
+						</p>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default AnimeCategoriesPageView;
